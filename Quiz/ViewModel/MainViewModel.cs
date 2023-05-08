9bba8f1d43da1e361ecd4a507282bf4e9b7c0b35
Avenek <j.machnik2002@gmail.com>
2023-05-08 22:00:35 +0200

    First commit

---
+++ Quiz/ViewModel/MainViewModel.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.Win32;
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Transactions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Quiz.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+        {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private readonly Model.MainModel model = new Model.MainModel();
+
+        private int selectedQuiz = 0;
+        public int SelectedQuiz
+        {
+            get => selectedQuiz;
+            set
+            {
+                selectedQuiz = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedQuiz)));
+            }
+        }
+        private List<string> quizes;
+        public List<string> Quizes
+        {
+            get => quizes;
+            set
+            {
+                quizes = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedQuiz)));
+            }
+        }
+        private Visibility quizListVisibility;
+        public Visibility QuizListVisibility
+        {
+            get { return quizListVisibility; }
+            set
+            {
+                quizListVisibility = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(QuizListVisibility)));
+            }
+        }
+        private Visibility startQuizVisibility;
+        public Visibility StartQuizVisibility
+        {
+            get { return startQuizVisibility; }
+            set
+            {
+                startQuizVisibility = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(StartQuizVisibility)));
+            }
+        }
+
+
+        public MainViewModel()
+        {
+
+        }
+
+        private ICommand startQuiz;
+        public ICommand StartQuiz
+        {
+
+            get
+            {
+                if (startQuiz == null)
+                    startQuiz = new RelayCommand(
+
+                     (o) =>
+                     {
+
+                         Frame frame = Application.Current.MainWindow.FindName("MainFrame") as Frame;
+
+
+                         if (frame != null)
+                         {
+                             QuizListVisibility = Visibility.Collapsed;
+                             StartQuizVisibility = Visibility.Collapsed;
+                             frame.Navigate(new QuizPage());
+                         }
+                     }
+                    ,
+                    (o) => true
+                    );
+                return startQuiz;
+            }
+        }
+        private ICommand selectQuiz;
+        public ICommand SelectQuiz
+        {
+
+            get
+            {
+                if (selectQuiz == null)
+                    selectQuiz = new RelayCommand(
+
+                     (o) =>
+                     {
+
+                     }
+                    ,
+                    (o) => true
+                    );
+                return selectQuiz;
+            }
+        }
+    }
+}
---
+++ Quiz/ViewModel/RelayCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace Quiz.ViewModel
+{
+
+        public class RelayCommand : ICommand
+        {
+           public event EventHandler CanExecuteChanged
+           {
+                add
+                {
+                    if (canExecute != null) CommandManager.RequerySuggested += value;
+                }
+                remove
+                {
+                    if (canExecute != null) CommandManager.RequerySuggested -= value;
+                }
+            }
+
+            private Action<object> execute;
+            private Predicate<object> canExecute;
+
+            public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+            {
+                this.execute = execute;
+                this.canExecute = canExecute;
+            }
+
+
+            public bool CanExecute(object parameter) => canExecute == null ? true : canExecute(parameter);
+
+            public void Execute(object parameter) => execute(parameter);
+        }
+}
